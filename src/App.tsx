fcb04a059e7c596aa41b69a54c264c9b7a6191e2
Mariana <marianapertuzv@gmail.com>
2022-10-17 18:08:08 -0500

    feat: added button logic for back and next

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,17 @@ import { useMultistepForm } from './useMultistepForm'
 
 function App() {
 
-  const { steps, currentStepIndex, step } = useMultistepForm([<div>One</div>, <div>Two</div>])
+  const { steps, currentStepIndex, step, isFirstStep, isLastStep, back, next } = useMultistepForm([<div>One</div>, <div>Two</div>, <div>Three</div>])
 
   return (
     <div className={styles.container}>
       <form>
         <div className={styles.number}>{currentStepIndex + 1} / {steps.length}</div>
         {step}
+        <div className={styles.buttonContainer}>
+        {!isFirstStep && <button type='button' onClick={back}>Back</button>}
+        <button type='button' onClick={next}>{!isLastStep ? 'Next' : 'Finish'}</button>
+      </div>
       </form>
       
     </div>
