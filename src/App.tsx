8745269a66dc464da0cd00f1e1726c646bf99aac
Mariana <marianapertuzv@gmail.com>
2022-10-17 17:44:26 -0500

    style: add styling to container

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
 import { useState } from 'react'
-import './App.css'
+import styles from './styles.module.css'
+import { useMultistepForm } from './useMultistepForm'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const { steps, currentStepIndex, step } = useMultistepForm([<div>One</div>, <div>Two</div>])
 
   return (
-    <h1>hola</h1>
+    <div className={styles.container}>
+      <form>
+        <div className={styles.number}>{currentStepIndex + 1} / {steps.length}</div>
+        {step}
+      </form>
+      
+    </div>
   )
 }
 
